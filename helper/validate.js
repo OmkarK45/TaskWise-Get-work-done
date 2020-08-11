7d668c5dead38139180d226d31355b3bd98a3d09
omkark45 <omkarak45@gmail.com>
2020-08-11 23:17:08 +0530

    Added custom validation text for email and password.

--- helper/validate.js
+++ helper/validate.js
@@ -6,8 +6,16 @@ const userValidSchema = Joi.object({
          "string.empty": `Username cannot be an empty field`,
          "any.required": `Username is a required.`
                  }),
-    email: Joi.string() .min(5) .required() .email(),
-    password: Joi.string() .min(6) .required() })
+    email: Joi.string() .min(5) .required() .email().messages({ 
+        "string.base": `Not a valid email :P`,
+         "string.empty": `Email field cannnot be empty.`,
+         "any.required": `Email is required.`
+                 }),
+    password: Joi.string() .min(6) .required().messages({ 
+        "string.base": `Invalid characters in password.`,
+         "string.empty": `Password cannot be empty.`,
+         "any.required": `Password is required.`
+                 }) })
 
 module.exports = {
     userValidSchema
