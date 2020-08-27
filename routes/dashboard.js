c55bd04b4e76d4817b96bc4f515362ae18c83bd3
omkark45 <omkarak45@gmail.com>
2020-08-27 17:33:00 +0530

    [FIX] Correctly render task in EJS {Must loop to show all cards though}

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -11,11 +11,12 @@ const Joi = require('@hapi/joi');
 router.get("/", ensureAuthenticated,async (req, res) => {
   try{
       const posts = await Post.find({userID:req.user._id});
-      // res.render('dashboard',{user:req.user, posts:posts});
-      res.json({
-        user:req.user,
-        posts:posts
-      })
+      res.render('dashboard',{user:req.user, posts:posts});
+      console.log(posts[0].title)
+      // res.json({
+      //   user:req.user,
+      //   posts:posts
+      // })
       // res.render('dashboard',{userPost : posts})
   }
   catch(err){
@@ -23,6 +24,75 @@ router.get("/", ensureAuthenticated,async (req, res) => {
   }
   
 });
+
+
+router.post('/new',ensureAuthenticated,async (req,res)=>{
+  const post = new Post({
+      title:req.body.title,
+      description:req.body.description,
+      userID:req.user._id,
+      subtitle:req.body.subtitle,
+      category:req.body.category,
+      tags:req.body.tags,
+      time:req.body.time,
+      image:req.body.image
+  });
+  try{
+      const savedPost = await post.save()
+      res.json({savedPost})
+  }
+ catch(err){
+  res.json({message:err})
+ }
+    
+});
+
+// Specific post
+router.get('/:postId', ensureAuthenticated,async (req,res)=>{
+  try{
+      const post = await Post.findById(req.params.postId);
+      res.json(post);
+  }
+  catch(err){
+      res.json({message:err});
+  }
+});
+
+// Route for searching particular task
+router.post('/search/:query', ensureAuthenticated,async(req,res)=>{
+  try{
+      const post = await Post.find({title:req.params.query})
+      res.json(post);
+  }catch(err){
+      res.json({
+          message:'Requested document was not found in database.'
+      });
+  }
+})
+
+
+// Delete a specific post
+router.delete('/:postId', ensureAuthenticated,async (req,res)=>{
+  try{
+      const removedPost = await Post.deleteOne({_id:req.params.postId});
+  }
+  catch(err){
+      res.json({message:err})
+  }
+});
+
+// Update a post
+router.patch('/:postId',ensureAuthenticated,async (req,res)=>{
+  try{
+     const updatedPost = await Post.updateOne({_id:req.params.postId},{$set:{title:req.body.title}}
+      );
+      res.json(updatedPost);
+  }
+  catch(err){
+      res.json({message:err});
+  }
+})
+
 
 
 // // Dashboard PROTECTED ROUTE
