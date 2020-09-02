e38b727747fc50e60237ff2f82ea70656ce49ef9
omkark45 <omkarak45@gmail.com>
2020-09-02 16:39:55 +0530

    Removed scroll detector [--]

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,15 @@
 var nav = document.getElementById('site-menu');
 var header = document.getElementById('top');
 
-window.addEventListener('scroll', function() {
-  if (window.scrollY >=400) { // adjust this value based on site structure and header image height
-    nav.classList.add('nav-sticky');
-    header.classList.add('pt-scroll');
-  } else {
-    nav.classList.remove('nav-sticky');
-    header.classList.remove('pt-scroll');
-  }
-});
+// window.addEventListener('scroll', function() {
+//   if (window.scrollY >=400) { // adjust this value based on site structure and header image height
+//     nav.classList.add('nav-sticky');
+//     header.classList.add('pt-scroll');
+//   } else {
+//     nav.classList.remove('nav-sticky');
+//     header.classList.remove('pt-scroll');
+//   }
+// });
 
 function navToggle() {
         var btn = document.getElementById('menuBtn');
