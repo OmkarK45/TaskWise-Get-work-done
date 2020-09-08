57e8e23ae3a92e7e2c5a81cc571033fbb4d32fbc
omkark45 <omkarak45@gmail.com>
2020-09-08 16:32:52 +0530

    New Modal layour thanks to Sushil, New datepicker

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
-var nav = document.getElementById('site-menu');
-var header = document.getElementById('top');
+var nav = document.getElementById("site-menu");
+var header = document.getElementById("top");
 
 // window.addEventListener('scroll', function() {
 //   if (window.scrollY >=400) { // adjust this value based on site structure and header image height
@@ -12,19 +12,19 @@ var header = document.getElementById('top');
 // });
 
 function navToggle() {
-        var btn = document.getElementById('menuBtn');
-        var nav = document.getElementById('menu');
-        var menuIcon = document.getElementById('menuIcon');
-        if(menuIcon.classList.contains('fa-bars')){
-            console.log('Yes Bars');
-            menuIcon.classList.remove('fa-bars');
-            menuIcon.classList.add('fa-times');
-          }else{
-            menuIcon.classList.add('fa-bars');
-            menuIcon.classList.remove('fa-times');
-          }
-        btn.classList.toggle('open');
-        nav.classList.toggle('flex');
-        nav.classList.toggle('items-center');
-        nav.classList.toggle('hidden');
-    }
+  var btn = document.getElementById("menuBtn");
+  var nav = document.getElementById("menu");
+  var menuIcon = document.getElementById("menuIcon");
+  if (menuIcon.classList.contains("fa-bars")) {
+    console.log("Yes Bars");
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-times");
+  } else {
+    menuIcon.classList.add("fa-bars");
+    menuIcon.classList.remove("fa-times");
+  }
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("items-center");
+  nav.classList.toggle("hidden");
+}
