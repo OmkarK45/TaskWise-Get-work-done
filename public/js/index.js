ecaaa919ef7a5f4ea7220e47de5b79525af63f6c
omkark45 <omkarak45@gmail.com>
2020-09-10 20:16:44 +0530

    [Code Cleanup] Removed Commented Code

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,5 @@
 var nav = document.getElementById("site-menu");
 var header = document.getElementById("top");
-
-// window.addEventListener('scroll', function() {
-//   if (window.scrollY >=400) { // adjust this value based on site structure and header image height
-//     nav.classList.add('nav-sticky');
-//     header.classList.add('pt-scroll');
-//   } else {
-//     nav.classList.remove('nav-sticky');
-//     header.classList.remove('pt-scroll');
-//   }
-// });
-
 function navToggle() {
   var btn = document.getElementById("menuBtn");
   var nav = document.getElementById("menu");
